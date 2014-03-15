af68505f350aa3406772460dd87b4a668d7573bc
Jeff McLean <jeff.t.mclean@gmail.com>
2014-03-15 16:50:01 -0400

    javascript for experience bar and point ticker, updated styling for exp. bar and control panel

---
+++ javascript/points.js
@@ -0,0 +1,35 @@
+var level = {
+  current: 1,
+  score: 0,
+  base: 0,
+  next: 30
+};
+
+var $expCountdown = $('#exp-countdown');
+
+function pointIncreaser(targetScore,duration){
+
+  //tick through total points
+  var $totalScore = $('#total-score');
+
+  var delay = duration / (targetScore - level.score);
+
+  setTimeout(function pointLoop(){
+    $totalScore.html(level.score);
+    $expCountdown.html(level.next - level.score + " to level " + (level.current + 1));
+    if(level.score < targetScore){
+      level.score++;
+      setTimeout(pointLoop,delay);
+    }
+  }, 0);
+
+  $('#experience').animate({
+    width: 100 * (targetScore - level.base) / (level.next - level.base) + "%"
+  }, duration);
+
+}
+
+$(document).ready(function(){
+  $('#exp-countdown').html(level.next - level.score + " to level " + (level.current + 1));
+  pointIncreaser(24,2000);
+})
