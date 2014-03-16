41716502a35a7ece93c085466966a6a05591fbb2
Jeff McLean <jeff.t.mclean@gmail.com>
2014-03-16 11:42:14 -0400

    restructured js document to prevent loading order issues

--- javascript/interface.js
+++ javascript/interface.js
@@ -1,11 +1,15 @@
+$(document).ready(function(){
+  
+var $columns = $('#grid .column');
+var $row = $('#grid .row');
+var snapX = {};
+var snapY = {};
+var halfColumn = $columns.width()/2;
+var halfRow = $row.height()/2;
+var activeColumn;
+
 function place(){
   var $pepActive = $('.pep-active');
-  var $columns = $('#grid .column');
-  var $row = $('#grid .row');
-  var snapX = {};
-  var snapY = {};
-  var halfColumn = $columns.width()/2;
-  var halfRow = $row.height()/2;
   snapX.dx = halfColumn;
   snapY.dy = halfRow;
   $columns.each(function(){
@@ -14,7 +18,7 @@ function place(){
     if($this.dx <= halfColumn){
       snapX = $this;
       $pepActive.offset({left:snapX.offset().left});
-      $pepActive.toggleClass('no-bg');
+      $pepActive.addClass('no-bg');
       var $rows = $this.children('.row');
       $rows.each(function(){
         $this = $(this);
@@ -29,11 +33,18 @@ function place(){
   if(snapX.dx >= halfColumn || snapY.dy >= halfRow){
     $pepActive.remove();
   } else {
-    snapX.toggleClass('active-column');
+    activeColumn = snapX;
+    snapX.addClass('active-column');
   }
 }
 
-function pickup(){};
+function pickup(){
+  var $pepActive = $('.pep-active');
+  $pepActive.removeClass('no-bg');
+  if(activeColumn != null){
+    activeColumn.removeClass('active-column');
+  }
+};
 
 function initializePep(){
   $('.pep').pep({
@@ -43,3 +54,6 @@ function initializePep(){
     shouldEase: false
   });
 };
+initializePep();
+
+});
