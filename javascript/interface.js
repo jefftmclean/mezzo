905eeb605411495b7b1732a792027bdfc924613f
Jeff McLean <jeff.t.mclean@gmail.com>
2014-03-16 12:20:01 -0400

    fraction blocks won't bleed off grid edge

--- javascript/interface.js
+++ javascript/interface.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  
+
 var $columns = $('#grid .column');
 var $row = $('#grid .row');
 var snapX = {};
@@ -7,15 +7,42 @@ var snapY = {};
 var halfColumn = $columns.width()/2;
 var halfRow = $row.height()/2;
 var activeColumn;
+var $half = $('#source-container .source .half').clone();
+var $fourth = $('#source-container .source .fourth').clone();
+var $eighth = $('#source-container .source .eighth').clone();
 
 function place(){
   var $pepActive = $('.pep-active');
+  //generate replacement block
+  if( $pepActive.attr("data-origin") === "true" ){
+    switch($pepActive.attr("data-duration")){
+      case "4":
+        $("#source-container>.half").prepend($half.clone());
+        initializePep();
+        break;
+      case "2":
+        $("#source-container>.fourth").prepend($fourth.clone());
+        initializePep();
+        break;
+      case "1":
+        $("#source-container>.eighth").prepend($eighth.clone());
+        initializePep();
+        break;
+      default:
+        break;
+    }
+    $pepActive.attr("data-origin", "false");
+  }
+  //snap block to grid
   snapX.dx = halfColumn;
   snapY.dy = halfRow;
+  var columnIndex = 0;
   $columns.each(function(){
     $this = $(this);
     $this.dx = Math.abs($this.offset().left - $pepActive.offset().left);
-    if($this.dx <= halfColumn){
+    console.log(columnIndex);
+    console.log(parseInt($pepActive.attr("data-duration")));
+    if($this.dx <= halfColumn && columnIndex + parseInt($pepActive.attr("data-duration")) <= $columns.length){
       snapX = $this;
       $pepActive.offset({left:snapX.offset().left});
       $pepActive.addClass('no-bg');
@@ -29,6 +56,7 @@ function place(){
         }
       });
     }
+    columnIndex += 1;
   });
   if(snapX.dx >= halfColumn || snapY.dy >= halfRow){
     $pepActive.remove();
